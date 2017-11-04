58067ef430a0b0e9bc0ed7fba4913d62f8871cd8
Bilal Tariq <bilal-tariq@live.com>
2017-11-05 00:28:08 +0800

    modified channel/room.js to recieve data and display

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,37 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+    // console.log(data)
+
+    if (data.typing !== null && data.user_id !== parseInt(document.getElementById('user-id').innerHTML)){
+      if (data.typing === "true"){
+        $(".typing-text").remove();
+        $('#messages').append ("<typing-text class='typing-text'>" + "Typing.... " + "</br>" + "</br>" + "</typing-text>")
+      }else{
+        $(".typing-text").remove();
+      }
+    }
+
+    if (data.typing === undefined){
+      if (data.user_id === parseInt(document.getElementById('user-id').innerHTML)){
+        $('#messages').append ("<p style='text-align: right;text-align: right;background: rgba(0, 128, 128, 0.38);padding: 10px;border-radius: 6px;'>" + data.content +  "</br>" + "<created_at style='font-size:12px;'>" + data.created_at + "</created_at>" + "</p>");
+      }else{
+        $('#messages').append ("<p style='float:lefttext-align: right;background: rgba(245, 222, 179, 0.51);padding: 10px;border-radius: 6px;'>" + data.content + "</br>" + "<created_at style='font-size:12px;'>" + data.created_at + "</created_at>" + "</p>");
+      }
+    };
+  },
+
+  speak: function(message) {
+    this.perform('speak', {
+      message: message
+    });
+  }
+});
