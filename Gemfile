a22f233ad37fe872d0014b61a38ccadc6282d835
Bilal Tariq <bilal-tariq@live.com>
2017-11-05 19:07:51 +0800

    testing action cable for production

--- Gemfile
+++ Gemfile
@@ -54,7 +54,6 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'redis'
   gem 'hiredis'
-  gem 'em-synchrony'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://aqueous-mountain-36981.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = [ 'https://aqueous-mountain-36981.herokuapp.com', 'http://aqueous-mountain-36981.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
