a06dae2cf7fa7bd8e11f37d08c752621d22b8ead
Bilal Tariq <bilal-tariq@live.com>
2017-11-18 13:46:02 +0800

    refactored dirty code ( previous code was quickly done)

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,12 +9,40 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
     #Data comes here from the server and is passes to channel/room.js to be brodcasted
-    subscriber = Subscriber.find(data['message']['subscriber_id'])
-    if !data['message']['typing'].present?
-      message = Message.create(content: data['message']['content'], subscriber_id: subscriber.id)
-      ActionCable.server.broadcast 'room_channel', {content: data['message']['content'], user_id: subscriber.user_id, created_at: message.created_at.strftime("%a %e %b %l:%M %p")}
+    set_incomming_message(data)
+    set_subscriber
+    if @message['typing'].blank?
+      create_message
+      broadcast_content = create_broadcast_content_hash(typing: false)
     else
-      ActionCable.server.broadcast 'room_channel', {typing: data['message']['typing'], user_id: subscriber.user_id}
+      broadcast_content = create_broadcast_content_hash(typing: true)
     end
+    broadcast_contant(broadcast_content)
+  end
+
+  private
+
+  def set_incomming_message(data)
+    @message = data['message']
+  end
+
+  def set_subscriber
+    @subscriber = Subscriber.find(@message['subscriber_id'])
+  end
+
+  def create_message
+    @new_message = Message.create(content: @message['content'], subscriber_id: @subscriber.id)
+  end
+
+  def create_broadcast_content_hash(typing:)
+    if typing
+      {typing: @message['typing'], user_id: @subscriber.user_id}
+    else
+      {content: @new_message.content, user_id: @subscriber.user_id, created_at: @new_message.created_at.strftime("%a %e %b %l:%M %p")}
+    end
+  end
+
+  def broadcast_contant(broadcast_content)
+    ActionCable.server.broadcast 'room_channel', broadcast_content
   end
 end
